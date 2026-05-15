f33cec54e25552b559af00ab18a441e87950a192
Effently <qaweemm@protonmail.com>
2026-05-15 13:40:14 +0300

    fix

--- app/src/main/java/me/effently/moddedmcpe/fragments/gameManager/InstancesFragment.kt
+++ app/src/main/java/me/effently/moddedmcpe/fragments/gameManager/InstancesFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
-import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -16,6 +15,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import me.effently.moddedmcpe.InitializingActivity
 import me.effently.moddedmcpe.R
+import me.effently.moddedmcpe.utils.InstanceUIHelper
 import org.endercore.android.EnderCore
 import org.endercore.android.operator.instance.InstanceOperator
 import org.endercore.android.operator.instance.model.GameInstance
@@ -88,7 +88,7 @@ class InstancesFragment : Fragment() {
     }
 
     private fun retryDownload(instance: GameInstance) {
-        me.effently.moddedmcpe.utils.InstanceUIHelper.retryDownload(this, operator, instance) {
+        InstanceUIHelper.retryDownload(this, operator, instance) {
             loadInstances()
         }
     }
--- app/src/main/java/me/effently/moddedmcpe/fragments/gameManager/VersionsFragment.kt
+++ app/src/main/java/me/effently/moddedmcpe/fragments/gameManager/VersionsFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -14,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import me.effently.moddedmcpe.InitializingActivity
 import me.effently.moddedmcpe.R
+import me.effently.moddedmcpe.utils.InstanceUIHelper
 import org.endercore.android.EnderCore
 import org.endercore.android.operator.instance.InstanceOperator
 import org.endercore.android.operator.instance.RemoteVersionRepository
@@ -100,7 +100,7 @@ class VersionsFragment : Fragment() {
     }
 
     private fun startDownload(version: RemoteVersion) {
-        me.effently.moddedmcpe.utils.InstanceUIHelper.startDownload(this, operator, version) {
+        InstanceUIHelper.startDownload(this, operator, version) {
             // success
         }
     }
