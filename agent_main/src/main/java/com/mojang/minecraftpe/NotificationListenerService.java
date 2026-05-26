6ac7dcf275836018d3ac5905ae38ad4218e47e79
Effently <qaweemm@protonmail.com>
2026-05-27 02:53:01 +0300

    feat: 1.19+ stuff #2

---
+++ agent_main/src/main/java/com/mojang/minecraftpe/NotificationListenerService.java
@@ -0,0 +1,19 @@
+package com.mojang.minecraftpe;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class NotificationListenerService extends Service {
+
+    public native void nativePushNotificationReceived(int type, String title, String description, String data);
+
+    public static String getDeviceRegistrationToken() {
+        return "";
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
